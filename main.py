73eaa2175f81c904e9b3650c82dc6d21e4c5282a
Rameez <rameez007rz@gmail.com>
2020-12-30 04:19:10 +0530

    added mouse hover effect

--- main.py
+++ main.py
@@ -7,6 +7,192 @@ A = 0
 operator= ""
 
 
+def entered_7(event):
+    btn7.config(
+        bg="#C9C9C9",
+        fg="#ffffff"
+    )
+def left_7(event):
+    btn7.config(
+        bg="#F0F0F0",
+        fg="#000000"
+    )
+
+def entered_8(event):
+    btn8.config(
+        bg="#C9C9C9",
+        fg="#ffffff"
+    )
+def left_8(event):
+    btn8.config(
+        bg="#F0F0F0",
+        fg="#000000"
+    )
+
+def entered_9(event):
+    btn9.config(
+        bg="#C9C9C9",
+        fg="#ffffff"
+    )
+def left_9(event):
+    btn9.config(
+        bg="#F0F0F0",
+        fg="#000000"
+    )
+def entered_plus(event):
+    btn_plus.config(
+        bg="#C9C9C9",
+        fg="#ffffff"
+    )
+def left_plus(event):
+    btn_plus.config(
+        bg="#F0F0F0",
+        fg="#000000"
+    )
+
+def entered_4(event):
+    btn4.config(
+        bg="#C9C9C9",
+        fg="#ffffff"
+    )
+def left_4(event):
+    btn4.config(
+        bg="#F0F0F0",
+        fg="#000000"
+    )
+
+def entered_5(event):
+    btn5.config(
+        bg="#C9C9C9",
+        fg="#ffffff"
+    )
+def left_5(event):
+    btn5.config(
+        bg="#F0F0F0",
+        fg="#000000"
+    )
+
+def entered_6(event):
+    btn6.config(
+        bg="#C9C9C9",
+        fg="#ffffff"
+    )
+def left_6(event):
+    btn6.config(
+        bg="#F0F0F0",
+        fg="#000000"
+    )
+
+def entered_minus(event):
+    btn_minus.config(
+        bg="#C9C9C9",
+        fg="#ffffff"
+    )
+def left_minus(event):
+    btn_minus.config(
+        bg="#F0F0F0",
+        fg="#000000"
+    )
+def entered_1(event):
+    btn1.config(
+        bg="#C9C9C9",
+        fg="#ffffff"
+    )
+def left_1(event):
+    btn1.config(
+        bg="#F0F0F0",
+        fg="#000000"
+    )
+
+def entered_2(event):
+    btn2.config(
+        bg="#C9C9C9",
+        fg="#ffffff"
+    )
+def left_2(event):
+    btn2.config(
+        bg="#F0F0F0",
+        fg="#000000"
+    )
+
+def entered_3(event):
+    btn3.config(
+        bg="#C9C9C9",
+        fg="#ffffff"
+    )
+def left_3(event):
+    btn3.config(
+        bg="#F0F0F0",
+        fg="#000000"
+    )
+
+def entered_multi(event):
+    btn_multi.config(
+        bg="#C9C9C9",
+        fg="#ffffff"
+    )
+def left_multi(event):
+    btn_multi.config(
+        bg="#F0F0F0",
+        fg="#000000"
+    )
+
+def entered_clear(event):
+    btn_clear.config(
+        bg="#C9C9C9",
+        fg="#ffffff"
+    )
+def left_clear(event):
+    btn_clear.config(
+        bg="#F0F0F0",
+        fg="#000000"
+    )
+
+def entered_zero(event):
+    btn_zero.config(
+        bg="#C9C9C9",
+        fg="#ffffff"
+    )
+def left_zero(event):
+    btn_zero.config(
+        bg="#F0F0F0",
+        fg="#000000"
+    )
+
+def entered_result(event):
+    btn_result.config(
+        bg="#C9C9C9",
+        fg="#ffffff"
+    )
+def left_result(event):
+    btn_result.config(
+        bg="#F0F0F0",
+        fg="#000000"
+    )
+
+def entered_division(event):
+    btn_division.config(
+        bg="#C9C9C9",
+        fg="#ffffff"
+    )
+def left_division(event):
+    btn_division.config(
+        bg="#F0F0F0",
+        fg="#000000"
+    )
+
+
+
+
+
+
+
+
+
+
+
+
+
 def btn_1_is_clicked():
     global val
     val = val + "1"
@@ -166,6 +352,8 @@ btn_row_3.pack(expand=True, fill="both", )
 btn_row_4 = Frame(root)
 btn_row_4.pack(expand=True, fill="both", )
 
+
+
 btn7 = Button(
     btn_row_1,
     text="7",
@@ -177,6 +365,8 @@ btn7 = Button(
 btn7.pack(
     side=LEFT, expand=True, fill='both'
 )
+btn7.bind("<Enter>", entered_7)
+btn7.bind("<Leave>", left_7)
 
 btn8 = Button(
     btn_row_1,
@@ -189,6 +379,8 @@ btn8 = Button(
 btn8.pack(
     side=LEFT, expand=True, fill='both'
 )
+btn8.bind("<Enter>", entered_8)
+btn8.bind("<Leave>", left_8)
 
 btn9 = Button(
     btn_row_1,
@@ -201,6 +393,8 @@ btn9 = Button(
 btn9.pack(
     side=LEFT, expand=True, fill='both'
 )
+btn9.bind("<Enter>", entered_9)
+btn9.bind("<Leave>", left_9)
 
 btn_plus = Button(
     btn_row_1,
@@ -213,6 +407,8 @@ btn_plus = Button(
 btn_plus.pack(
     side=LEFT, expand=True, fill='both'
 )
+btn_plus.bind("<Enter>", entered_plus)
+btn_plus.bind("<Leave>", left_plus)
 
 btn4 = Button(
     btn_row_2,
@@ -225,6 +421,8 @@ btn4 = Button(
 btn4.pack(
     side=LEFT, expand=True, fill='both'
 )
+btn4.bind("<Enter>", entered_4)
+btn4.bind("<Leave>", left_4)
 
 btn5 = Button(
     btn_row_2,
@@ -237,6 +435,8 @@ btn5 = Button(
 btn5.pack(
     side=LEFT, expand=True, fill='both'
 )
+btn5.bind("<Enter>", entered_5)
+btn5.bind("<Leave>", left_5)
 
 btn6 = Button(
     btn_row_2,
@@ -249,6 +449,8 @@ btn6 = Button(
 btn6.pack(
     side=LEFT, expand=True, fill='both'
 )
+btn6.bind("<Enter>", entered_6)
+btn6.bind("<Leave>", left_6)
 
 btn_minus = Button(
     btn_row_2,
@@ -261,6 +463,8 @@ btn_minus = Button(
 btn_minus.pack(
     side=LEFT, expand=True, fill='both'
 )
+btn_minus.bind("<Enter>", entered_minus)
+btn_minus.bind("<Leave>", left_minus)
 
 btn1 = Button(
     btn_row_3,
@@ -273,6 +477,9 @@ btn1 = Button(
 btn1.pack(
     side=LEFT, expand=True, fill='both',
 )
+btn1.bind("<Enter>", entered_1)
+btn1.bind("<Leave>", left_1)
+
 
 btn2 = Button(
     btn_row_3,
@@ -285,6 +492,8 @@ btn2 = Button(
 btn2.pack(
     side=LEFT, expand=True, fill='both'
 )
+btn2.bind("<Enter>", entered_2)
+btn2.bind("<Leave>", left_2)
 
 btn3 = Button(
     btn_row_3,
@@ -297,6 +506,8 @@ btn3 = Button(
 btn3.pack(
     side=LEFT, expand=True, fill='both'
 )
+btn3.bind("<Enter>", entered_3)
+btn3.bind("<Leave>", left_3)
 
 btn_multi = Button(
     btn_row_3,
@@ -309,6 +520,8 @@ btn_multi = Button(
 btn_multi.pack(
     side=LEFT, expand=True, fill='both'
 )
+btn_multi.bind("<Enter>", entered_multi)
+btn_multi.bind("<Leave>", left_multi)
 
 btn_clear = Button(
     btn_row_4,
@@ -321,6 +534,8 @@ btn_clear = Button(
 btn_clear.pack(
     side=LEFT, expand=True, fill='both'
 )
+btn_clear.bind("<Enter>", entered_clear)
+btn_clear.bind("<Leave>", left_clear)
 
 btn_zero = Button(
     btn_row_4,
@@ -334,6 +549,8 @@ btn_zero = Button(
 btn_zero.pack(
     side=LEFT, expand=True, fill='both'
 )
+btn_zero.bind("<Enter>", entered_zero)
+btn_zero.bind("<Leave>", left_zero)
 
 btn_result = Button(
     btn_row_4,
@@ -346,6 +563,8 @@ btn_result = Button(
 btn_result.pack(
     side=LEFT, expand=True, fill='both'
 )
+btn_result.bind("<Enter>", entered_result)
+btn_result.bind("<Leave>", left_result)
 
 btn_division = Button(
     btn_row_4,
@@ -358,5 +577,7 @@ btn_division = Button(
 btn_division.pack(
     side=LEFT, expand=True, fill='both'
 )
+btn_division.bind("<Enter>", entered_division)
+btn_division.bind("<Leave>", left_division)
 
 root.mainloop()
